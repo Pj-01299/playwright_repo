5165dda1f63dfe13f085f58424b3d45c17dd698b
Pj-01299 <pranav.jindal@cloudanalogy.com>
2026-04-01 00:30:22 +0530

    added all files

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'line',
-  reporter: process.env.CI ? 'github':[['list'],['html', { open: 'always' }],['line'],['json', { outputFile: 'test-results.json' }]],
+  reporter: "./custom_report/myReporter.ts",
+  // reporter: process.env.CI ? 'github':[['list'],['html', { open: 'always' }],['line'],['json', { outputFile: 'test-results.json' }]],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
